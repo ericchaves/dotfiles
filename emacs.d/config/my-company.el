d9783c0761250d0ee73e1acafddeccb483ea4d1f
Eric Chaves <eric@craftti.com.br>
2016-06-03 17:58:02 -0300

    refactored emacs

--- emacs.d/config/my-company.el
+++
@@ -1,9 +0,0 @@
-(use-package company
-  :ensure t
-  :bind
-  ("C-o" . company-complete)
-  :config
-  ;(global-set-key (kbd "<f1>") 'company-complete)
-  (add-hook 'prog-mode-hook 'company-mode))
-
-(provide 'my-company)
--- emacs.d/config/my-window-numbering.el
+++
@@ -1,8 +0,0 @@
-(require 'use-package)
-(use-package window-numbering
-  :ensure t
-  :config
-  (window-numbering-mode t))
-
-
-(provide 'my-window-numbering)
